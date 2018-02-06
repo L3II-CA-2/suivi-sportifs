d1f446fa874892323216f49e6f9ea839de5b5315
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-02-06 15:20:54 +0100

    Création de la classe sport et ajout des variable de la classe Sportif

---
+++ src/v1/Sport.java
@@ -0,0 +1,10 @@
+package v1;
+/**
+ * @author Kevin Kerboit
+ *
+ */
+public class Sport {
+	String sport;
+	
+	
+}
--- src/v1/Sportif.java
+++ src/v1/Sportif.java
@@ -3,10 +3,17 @@
  */
 package v1;
 
+import java.util.Date;
+
 /**
  * @author Kevin Kerboit
  *
  */
 public class Sportif {
-	String pseudo ;
+	String pseudo;
+	String nom;
+	String prenom;
+	Date datedenaissance;
+	Sport sport;
+	
 }
