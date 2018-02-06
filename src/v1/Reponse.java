0b9935d883b9dc57859bc0550295ea95a4e7b3af
ollivigl <ollivigl@localhost>
2018-02-06 15:31:26 +0100

    Debut de structure

--- src/v1/Reponse.java
+++ src/v1/Reponse.java
@@ -1,5 +1,17 @@
+/**
+ * 
+ */
 package v1;
 
+/**
+ * @author Glen Ollivier
+ *
+ */
 public class Reponse {
-
+	private Question _question;
+	private boolean _resultat;
+	Reponse(Question question, boolean resultat) {
+		_question = question;
+		_resultat = resultat;
+	} 
 }
