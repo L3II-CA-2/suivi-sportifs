20eecceaa9eab60050f4ca475719cfbd3a3f2ef3
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-02-06 15:32:09 +0100

    Ajout du constructeur plus test des longueur varaible

--- src/v1/Sportif.java
+++ src/v1/Sportif.java
@@ -10,10 +10,34 @@ import java.util.Date;
  *
  */
 public class Sportif {
-	String pseudo;
-	String nom;
-	String prenom;
-	Date datedenaissance;
-	Sport sport;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private Date datedenaissance;
+	private Sport sport;
+	
+	public Sportif(String pseudo, String nom, String prenom, Date datedenaissance, Sport sport) {
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.datedenaissance = datedenaissance;
+		this.sport = sport;
+		
+		//Vérification que le pseudo fasse plus de 5 caractères
+		if(pseudo.length() < 5) {
+			return;
+		}
+		
+		//Vérification que Nom et prénom fassent plus de 2 caractères chacun 
+		if(nom.length() < 2 && prenom.length() < 2) {
+			return;
+		}
+		
+		
+		
+		
+	}
+	
+	
 	
 }
