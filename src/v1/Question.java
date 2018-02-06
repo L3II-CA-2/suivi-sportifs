2d5a06d8de7f1ca8d574e0756dcd41b592345bcf
Valentin Pearce <valentin.pearce@forgetfulmuffin.fr>
2018-02-06 11:24:47 +0100

    Ajout de la structure pour les Questions

---
+++ src/v1/Question.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package v1;
+
+
+/**
+ * @author Valentin Pearce
+ * Superclasse Question
+ */
+public abstract class Question {
+	int id;
+	String intitule;
+}
---
+++ src/v1/Questionnaire.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package v1;
+
+import java.util.Date;
+
+/**
+ * @author Valentin Pearce
+ *
+ */
+public class Questionnaire {
+	int id;
+	Date debut;
+	Date fin;
+	ArrayList<Question> questions;
+}
