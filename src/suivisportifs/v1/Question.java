9716ee875e7e4efb9fca24e11dc4202ea82d67b7
Valentin Pearce <valentin.pearce@forgetfulmuffin.fr>
2018-02-12 18:37:33 +0100

    Récupération du fichier de Pierre

--- src/suivisportifs/v1/Question.java
+++ src/suivisportifs/v1/Question.java
@@ -1,14 +1,60 @@
 /**
  * 
  */
+
 package suivisportifs.v1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 
 /**
- * @author Valentin Pearce
- * Superclasse Question
+ * Question.
+ * @author Pierre Le Bras
+ *
  */
-public abstract class Question {
-	int id;
-	String intitule;
+public class Question {
+  private String intitule;
+  private boolean reponseDefault;
+
+  Question(String inttl, boolean rdflt) {
+    this.intitule = inttl;
+    this.reponseDefault = rdflt;
+    
+  }
+      
+  /**
+   * accesseur Intitulé Question.
+   */
+  public String getIntitule() {
+      
+    return this.intitule;
+      
+  }
+  
+  /**
+   * modificateur Intitulé Question.
+   */
+  public void setIntitule(String itl) {
+    
+    this.intitule = itl;
+    
+  }
+  
+  /**
+   * modificateur reponseDefault Question.
+   */
+  public void setReponseDefault(boolean reponseD) {
+    this.reponseDefault = reponseD;
+  }
+  
+  /**
+   * accesseur reponseDefault Question.
+   */
+  public boolean getReponseDefault() {
+    return this.reponseDefault;
+  }
+
 }
