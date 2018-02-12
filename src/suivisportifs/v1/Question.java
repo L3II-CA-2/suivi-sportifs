a9e11f436ae8461493e6f775678aaec257afb4f9
Licence3 II 2017-PEARCE Valentin <pearceva@localhost>
2018-02-12 14:20:29 +0100

    Revert "Merge remote-tracking branch 'origin/pierre' into valentin"
    This reverts commit 0073d3e22d4a5b6b9c724dbb5720026e983b1d89, reversing
    changes made to 68b4f11e49801548cdecb1f7f8b82801530a2b74.

---
+++ src/suivisportifs/v1/Question.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package suivisportifs.v1;
+
+
+/**
+ * @author Valentin Pearce
+ * Superclasse Question
+ */
+public abstract class Question {
+	int id;
+	String intitule;
+}
---
+++ src/suivisportifs/v1/QuestionOuiNon.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package suivisportifs.v1;
+
+
+/**
+ * @author Valentin Pearce
+ * Question Oui Non
+ */
+public class QuestionOuiNon extends Question{
+
+}
