6122e6d02022f5b7bd18be35c34d7f5040919fea
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-02-12 10:54:37 +0100

    Application de la norme CheckStyle sur les classe Sportif

--- src/suivisportifs/v1/Sportif.java
+++ src/suivisportifs/v1/Sportif.java
@@ -1,159 +1,200 @@
-/**
- * 
- */
 package suivisportifs.v1;
 
 import java.util.Date;
 import java.util.regex.*;
 
 /**
+ * Classe Sportif : Cette classe permet de definir un sportif.
+ * 
  * @author Kevin Kerboit
  *
  */
+
 public class Sportif {
-	private String pseudo;
-	private String nom;
-	private String prenom;
-	private Date datedenaissance;
-	private String sport;
-	
-	public Sportif() {
-		this.pseudo = null;
-		this.nom = null;
-		this.prenom = null;
-		this.datedenaissance = null;
-		this.sport = null;
-	}
+  private String pseudo;
+  private String nom;
+  private String prenom;
+  private Date datedenaissance;
+  private String sport;
+  
+  /**
+   * Constructeur.
+   */
+  
+  public Sportif() {
+    this.pseudo = null;
+    this.nom = null;
+    this.prenom = null;
+    this.datedenaissance = null;
+    this.sport = null;
+  }
 
-/*---------Pseudo---------*/
-	public String getPseudo() {
-		return pseudo;
-	}
+  /**
+   * Fonction d'accès à la variable Pseudo.
+   * 
+   */
+  
+  public String getPseudo() {
+    return pseudo;
+  }
 
-	public Boolean setPseudo(String pseudo) {
-		
-		Pattern pattern;
-	    Matcher matcher;
-	    Boolean res = true;
-	    
-	    pattern = Pattern.compile("[^\\w]");
-	    matcher = pattern.matcher(pseudo);
-	    
-	    if(matcher.find()){
-           res = false;
-        }
-		
-	    if(res ==false) {
-	    	 System.out.println("Erreur ! Le pseudo ne doit pas contenir d'accents ou des caractères spéciaux \n");
-	         return false;
-	    }
+  /** 
+   * Fonction pour modifier la variable pseudo.
+   *
+   */
+  
+  public Boolean setPseudo(String pseudo) {
+    
+    Pattern pattern;
+    Matcher matcher;
+    Boolean res = true;
+      
+    pattern = Pattern.compile("[^\\w]");
+    matcher = pattern.matcher(pseudo);
+      
+    if (matcher.find()) {
+      res = false;
+    }
+    
+    if (res == false) {
+      System.out.println("Erreur ! Le pseudo contient d'accents/caractères spéciaux \n");
+      return false;
+    }
 
-	    
-		if(pseudo.length() < 5) {
-			System.out.println("Erreur ! Le pseudo doit faire minimum 5 caractères ! \n");
-			return false;
-		}
-		
-		
-		this.pseudo = pseudo;
-		
-		System.out.println("Le pseudo est mis ! \n");
-		
-		return true;
-	}
+      
+    if (pseudo.length() < 5) {
+      System.out.println("Erreur ! Le pseudo doit faire minimum 5 caractères ! \n");
+      return false;
+    }
+    
+    
+    this.pseudo = pseudo;
+    
+    System.out.println("Le pseudo est mis ! \n");
+    
+    return true;
+  }
 
-/*---------Nom---------*/	
+  /**
+   * Fonction d'accès à la variable nom.
+   */
 
-	public String getNom() {
-		return nom;
-	}
+  public String getNom() {
+    return nom;
+  }
+  
+  /**
+   * Fonction de modification de la variable nom.
+   */
 
-	public Boolean setNom(String nom) {
-		
-		Pattern pattern;
-	    Matcher matcher;
-	    
-	    Boolean res = true;
-	     
-	    pattern = Pattern.compile("[^a-zA-Zéèêëöôäâàïîùûü]");
-	    matcher = pattern.matcher(nom);
-	    
-	    if(matcher.find()) {
-            res = false;
-        }
-		
-	    if(res ==false) {
-	    	 System.out.println("Erreur ! Le nom ne doit pas contenir de caractères spéciaux \n");
-	         return false;
-	    }
-	    
-		if(nom.length() < 2) {
-			System.out.println("Erreur ! Le nom doit faire minimum 2 caractères ! \n");
-			return false;
-		}
-		
-		this.nom = nom;
-		System.out.println("Le nom est mis ! \n");
-		return true;
-	}
+  public Boolean setNom(String nom) {
+    
+    Pattern pattern;
+    Matcher matcher;
+      
+    Boolean res = true;
+       
+    pattern = Pattern.compile("[^a-zA-Zéèêëöôäâàïîùûü]");
+    matcher = pattern.matcher(nom);
+      
+    if (matcher.find()) {
+      res = false;
+    }
+    
+    if (res == false) {
+      System.out.println("Erreur ! Le nom ne doit pas contenir de caractères spéciaux \n");
+      return false;
+    }
+      
+    if (nom.length() < 2) {
+      System.out.println("Erreur ! Le nom doit faire minimum 2 caractères ! \n");
+      return false;
+    }
+    
+    this.nom = nom;
+    System.out.println("Le nom est mis ! \n");
+    return true;
+  }
 
-/*---------Prenom---------*/	
-	
-	public String getPrenom() {
-		return prenom;
-	}
+  /**
+   * Fonction d'accès à la variable Prenom.
+   */
+  
+  public String getPrenom() {
+    return prenom;
+  }
+  
+  /**
+   * Modification de la variable Prenom.
+   */
 
-	public Boolean setPrenom(String prenom) {
-		
-		Pattern pattern;
-	    Matcher matcher;
-	    
-	    Boolean res =true;
-	     
-	    pattern = Pattern.compile("[^a-zA-Zéèêëöôäâàïîùûü]");
-	    matcher = pattern.matcher(prenom);
-	    
-	    if(matcher.find()) {
-            res = false;
-        }
-		
-	    if(res ==false) {
-	    	 System.out.println("Erreur ! Le prenom ne doit pas contenir de caractères spéciaux \n");
-	         return false;
-	    }
-	    
-		if(nom.length() < 2) {
-			System.out.println("Erreur ! Le nom doit faire minimum 2 caractères ! \n");
-			return null;
-		}
-		
-		this.prenom = prenom;
-		System.out.println("Le prenom est mis ! \n");
-		return true;
-	}
-	
-/*---------Date de naissance---------*/
+  public Boolean setPrenom(String prenom) {
+    
+    Pattern pattern;
+    Matcher matcher;
+      
+    Boolean res = true;
+       
+    pattern = Pattern.compile("[^a-zA-Zéèêëöôäâàïîùûü]");
+    matcher = pattern.matcher(prenom);
+      
+    if (matcher.find()) {
+      res = false;
+    }
+    
+    if (res == false) {
+      System.out.println("Erreur ! Le prenom ne doit pas contenir de caractères spéciaux \n");
+      return false;
+    }
+      
+    if (nom.length() < 2) {
+      System.out.println("Erreur ! Le nom doit faire minimum 2 caractères ! \n");
+      return null;
+    }
+    
+    this.prenom = prenom;
+    System.out.println("Le prenom est mis ! \n");
+    return true;
+  }
+  
+  /**
+   * Fonction d'accès de la variable Date de Naissance.
+   * @return
+   */
 
-	public Date getDatedenaissance() {
-		return datedenaissance;
-	}
+  public Date getDatedenaissance() {
+    return datedenaissance;
+  }
 
-	public void setDatedenaissance(Date datedenaissance) {
-		this.datedenaissance = datedenaissance;
-	}
+  /**
+   * Fonction de modification de la variable Date de Naissance.
+   *
+   */
+  
+  public void setDatedenaissance(Date datedenaissance) {
+    this.datedenaissance = datedenaissance;
+  }
 
-/*---------Sport---------*/
-	
-	public String getSport() {
-		return sport;
-	}
+  /**
+   * Fonction d'accès de la variable Sport.
+   * @return
+   */
+  
+  public String getSport() {
+    return sport;
+  }
 
-	public void setSport(String sport) {
-		this.sport = sport;
-	}
-	
-	
-	
-	
-	
+  /**
+   * Fonction de modification de la variable Sport.
+   * 
+   */
+  
+  public void setSport(String sport) {
+    this.sport = sport;
+  }
+  
+  
+  
+  
+  
 }
--- src/suivisportifs/v1/TestSportif.java
+++ src/suivisportifs/v1/TestSportif.java
@@ -1,28 +1,42 @@
 package suivisportifs.v1;
 
+
+
+/**
+ * Programme de test pour la classe Sportif.
+ * 
+ * @author Kevin Kerboit
+ *
+ */
+
 public class TestSportif {
 
-	public static void main(String[] args) {
-		// TODO Stub de la méthode généré automatiquement
-		
-		Sportif sp1 = new Sportif();
-		
-		if(sp1.setPseudo("SebLoeb1") == false) {
-			return;
-		}
-		
-		if(sp1.setNom("Loeb") == false) {
-			return;
-		}
-		
-		if(sp1.setPrenom("Sebastien&") == false) {
-			return;
-		}
-		
-		sp1.setSport("Rallye");
+  /**
+   * Fonction main du programme de test.
+   * 
+   */
 
-		
+  public static void main(String[] args) {
+    // TODO Stub de la méthode généré automatiquement
+    
+    Sportif sp1 = new Sportif();
+    
+    if (sp1.setPseudo("SebLoeb1") == false) {
+      return;
+    }
+    
+    if (sp1.setNom("Loeb") == false) {
+      return;
+    }
+    
+    if (sp1.setPrenom("Sebastien&") == false) {
+      return;
+    }
+    
+    sp1.setSport("Rallye");
 
-	}
+    
+
+  }
 
 }
