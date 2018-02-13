ab1d1e8be93ac1153d589de0f2d4ccafc5082bd2
ValentinPearce <valentin.pearce@forgetfulmuffin.fr>
2018-02-13 12:50:49 +0100

    Refactor des questionnaires

--- src/suivisportifs/v1/Questionnaire.java
+++ src/suivisportifs/v1/Questionnaire.java
@@ -1,97 +1,73 @@
 /**
- * 
+ *
  */
 
 package suivisportifs.v1;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Scanner;
-
 
 /**
  * Classe gérant la création et l'édition des questionnaires.
  * @author Valentin Pearce
- * 
+ *
  */
 public class Questionnaire {
-  private int id;
-  private static int idMaximum = 0;
+
   private String intitule;
   private Date debut;
   private Date fin;
   private ArrayList<Question> questions;
-  
-  private Questionnaire(int id, String intitule, Date debut, Date fin) {
-    this.id = id;
+
+  private Questionnaire(String intitule, Date debut, Date fin) {
     this.intitule = intitule;
     this.debut = debut;
     this.fin = fin;
-  }
-  
-  /**
-   * Accesseur de l'identifiant du questionnaire.
-   * @return l'identifiant du questionnaire
-   */
-  int getId() {
-    return id;
-  }
-
-  /**
-   * Modificateur de l'identifiant du questionnaire.
-   * @param id the id to set
-   */
-  private void setId(int id) {
-    this.id = id;
-  }
-
-  /**
-   * Accesseur du plus grand identifiant de questionnaire.
-   * Permet une incrémentation automatique des identifiants de questionnaires.
-   * @return Le plus grand identifiant de questionnaire
-   */
-  private static int getIdMaximum() {
-    return idMaximum;
-  }
-
-  
-  /**
-   * Modificateur du plus grand identifiant de questionnaire.
-   * Incrémente de 1 la valeur.
-   */
-  private static void incrementerIdMaximum() {
-    Questionnaire.idMaximum += 1;
   }
 
   /**
    * @return the intitule
    */
-  String getIntitule() {
+  public String getIntitule() {
     return intitule;
   }
 
   /**
    * @param intitule the intitule to set
    */
-  private void setIntitule(String intitule) {
-    this.intitule = intitule;
+   int setIntitule(String intitule) {
+     Date maintenant = new Date();
+     if (this.debut.after(maintenant)) {
+       this.intitule = intitule;
+       return 0;
+     }
+     return 1;
   }
 
   /**
-   * @return the debut
+   * @return la date debut
    */
-  Date getDebut() {
+  public Date getDebut() {
     return debut;
   }
 
   /**
+   * Permet de modfier la date de début du questionnaire
    * @param debut the debut to set
    */
-  private void setDebut(Date debut) {
-    this.debut = debut;
+  public int setDebut(Date debut) {
+    Date maintenant = new Date();
+    if (this.debut.after(maintenant)) {
+      GregorianCalendar validationDate = new GregorianCalendar();
+      validationDate.setTime(debut);
+      if (validationDate.get(GregorianCalendar.DAY_OF_WEEK) == GregorianCalendar.MONDAY) {
+        this.debut = debut;
+        return 0;
+      }
+      return 1;
+    }
+  return 2;
   }
 
   /**
@@ -104,75 +80,52 @@ public class Questionnaire {
   /**
    * @param fin the fin to set
    */
-  private void setFin(Date fin) {
-    this.fin = fin;
+  public int setFin(Date fin) {
+    Date maintenant = new Date();
+    if (this.debut.after(maintenant)) {
+      if (fin.after(this.debut)) {
+        GregorianCalendar validationDate = new GregorianCalendar();
+        validationDate.setTime(fin);
+        if (validationDate.get(GregorianCalendar.DAY_OF_WEEK) == GregorianCalendar.SUNDAY) {
+          this.fin = fin;
+          return 0;
+        }
+        return 1;
+      }
+      return 2;
+    }
+    return 3;
   }
 
-  public void ajouterQuestion(Question question){
-    this.questions.add(question);
+  /*
+   * Permet d'ajouter un instance de question à la liste de questions du questionnaire.
+   * @param Question initialisée
+   */
+  public int ajouterQuestion(Question question) {
+    Date maintenant = new Date();
+    if (this.debut.after(maintenant)) {
+      this.questions.add(question);
+      return 0;
+    }
+    return 1;
   }
-  
-  public static Questionnaire creer() {
-    incrementerIdMaximum();
-    
-    
-    boolean dateCorrecte = false;
-    SimpleDateFormat dateIso = new SimpleDateFormat("YYYY-MM-DD");
-    String dateEntree;
-    Date debut = new Date();
-    Date fin = new Date();
-    Date today = new Date();
-    GregorianCalendar verificationDate = new GregorianCalendar();
-    
-    System.out.println("Veuillez entrer l'intitulé du questionnaire :");
-    Scanner entreeClavier = new Scanner(System.in);
-    final String intitule = entreeClavier.nextLine();
-    while (!dateCorrecte) {
-      System.out.println("Veuillez entrer la date de début du questionnaire.");
-      System.out.println("Elle doit être au format AAAA-MM-JJ :");
-      dateEntree = entreeClavier.nextLine();
+
+  /*
+   * Permet de retirer une question.
+   * @return 0 ou code erreur
+   */
+  public int retirerQuestion(int index) {
+    Date maintenant = new Date();
+    if (this.debut.after(maintenant)) {
       try {
-        debut = dateIso.parse(dateEntree);
-        verificationDate.setTime(debut);
-        if (verificationDate.get(GregorianCalendar.DAY_OF_WEEK) == GregorianCalendar.MONDAY) {
-          if (debut.after(today)) {
-            dateCorrecte = true;
-          } else {
-            System.out.println("La date de début doit être après la date du jour.");
-          }
-        } else {
-          System.out.println("La date de début doit être un Lundi.");
-        }
-      } catch (ParseException e) {
-        System.out.println("Le format de la date doit respecter le format demandé.");
+        @SuppressWarnings("unused") // La question est retirée mais n'est pas utile par la suite.
+        Question questionSupprimee;
+        questionSupprimee = this.questions.remove(index);
+        return 0;
+      } catch (IndexOutOfBoundsException e) {
+        return 1;
       }
     }
-    dateCorrecte = false;
-    while (!dateCorrecte) {
-      System.out.println("Veuillez entrer la date de fin du questionnaire au format AAAA-MM-JJ :");
-      dateEntree = entreeClavier.nextLine();
-      try {
-        fin = dateIso.parse(dateEntree);
-        verificationDate.setTime(fin);
-        if (verificationDate.get(GregorianCalendar.DAY_OF_WEEK) == GregorianCalendar.SUNDAY) {
-          if (fin.after(today)) {
-            if (fin.after(debut)) {
-              dateCorrecte = true;
-            } else {
-              System.out.println("La date de fin doit être après la date de début.");
-            }
-          } else {
-            System.out.println("La date de fin doit être après la date du jour.");
-          }
-        } else {
-          System.out.println("La date de fin doit être un Dimanche.");
-        }
-      } catch (ParseException e) {
-        System.out.println("Le format de la date doit respecter le format demandé.");
-      }
-    }
-    entreeClavier.close();
-    int id = getIdMaximum();
-    return new Questionnaire(id, intitule, debut, fin);
+    return 2;
   }
 }
