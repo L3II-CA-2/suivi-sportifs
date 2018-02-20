b278ca5a2ef8e66da6defb6a845994553c587212
pinedeccrabe <pinedeccrabe@ubuntu>
2018-02-20 05:01:54 -0800

    Classe ListeReponses, ajout de fonctions de base.

--- src/suivisportifs/v1/ListeReponses.java
+++ src/suivisportifs/v1/ListeReponses.java
@@ -20,7 +20,44 @@ public class ListeReponses {
     }
     return null;
   }
-  public void add(Sportif sportif, Questionnaire questionnaire) {
-    
+  
+  public ReponsesSportifQuestionnaire add(Sportif sportif, Questionnaire questionnaire) {
+    ReponsesSportifQuestionnaire retours = this.get(sportif, questionnaire);
+    if (retours == null) {
+      retours = new ReponsesSportifQuestionnaire(sportif, questionnaire);
+    }
+    return retours;
+  }
+  
+  public boolean remove(ReponsesSportifQuestionnaire reponse) {
+    return this.reponses.remove(reponse);
+  }
+  
+  public int remove(Questionnaire questionnaire) {
+    int retours = 0;
+    Iterator<ReponsesSportifQuestionnaire> i = reponses.iterator();
+    ReponsesSportifQuestionnaire buffer;
+    while (i.hasNext()) {
+      buffer = i.next();
+      if (buffer.getQuestionnaire() == questionnaire) {
+        this.reponses.remove(buffer);
+        retours ++;
+      }
+    }
+    return retours;
+  }
+  
+  public int remove(Sportif sportif) {
+    int retours = 0;
+    Iterator<ReponsesSportifQuestionnaire> i = reponses.iterator();
+    ReponsesSportifQuestionnaire buffer;
+    while (i.hasNext()) {
+      buffer = i.next();
+      if (buffer.getSportif() == sportif) {
+        this.reponses.remove(buffer);
+        retours ++;
+      }
+    }
+    return retours;
   }
 }
