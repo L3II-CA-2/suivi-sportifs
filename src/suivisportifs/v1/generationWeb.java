e9e4793f1ef80b199346d4a7c3ca1953419a2c99
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-03-13 15:17:39 +0100

    pom.xml + fonction de génération html

---
+++ src/suivisportifs/v1/generationWeb.java
@@ -0,0 +1,15 @@
+package suivisportifs.v1;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupDir;
+
+public class generationWeb {
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    
+  }
+}
