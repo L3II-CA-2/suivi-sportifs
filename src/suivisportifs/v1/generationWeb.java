3df93e76f36a45c8b100314e3f05bbccc260c7a7
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-03-13 15:30:35 +0100

    Petit test de stringtemplate

--- src/suivisportifs/v1/generationWeb.java
+++ src/suivisportifs/v1/generationWeb.java
@@ -8,8 +8,18 @@ import org.stringtemplate.v4.STGroup;
 import org.stringtemplate.v4.STGroupDir;
 
 public class generationWeb {
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    
+  @SuppressWarnings("deprecation")
+  public static void main(String[] args) throws IOException {
+      STGroup g = new STGroupDir("src/suivisportifs/v1/WEB/template", '$','$');
+      ST index = g.getInstanceOf("accueil");
+      
+      index.add("quest_name", "Questionnaire d'initiation");
+      index.add("date_debut", "26/02/18");
+      index.add("date_fin", "04/03/18");
+      String result = index.render();
+      System.out.println(result);
+      
+      File newHtmlFile = new File("src/suivisportifs/v1/WEB/accueil.html");
+      FileUtils.writeStringToFile(newHtmlFile, result);
   }
 }
